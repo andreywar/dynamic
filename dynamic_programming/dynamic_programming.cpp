0330c808029a3b8bdc1d596c0b7f64f8fc0b28ee
andreywar <andryusha6@mail.ru>
2023-11-18 00:07:25 +0300

    just switch function calls bc in another subsequence its looks ugly

--- dynamic_programming/dynamic_programming.cpp
+++ dynamic_programming/dynamic_programming.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 
 int main() {
+	cout << "\n\n" << "Chain" << "\n\n";
+	chain_of_str();
 	cout << "Triangle_path" << "\n\n";
 	triangle();
 	cout << "\n\n" << "Degree_path" << "\n\n";
 	degree_();
-	cout << "\n\n" << "Chain" << "\n\n";
-	chain_of_str();
+	
 }
